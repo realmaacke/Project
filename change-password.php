9219ced4864943bac19393755902c20100706ae5
realmaacke <mackan.pettersson1@outlook.com>
2021-09-28 15:46:01 +0200

    followers

--- change-password.php
+++ change-password.php
@@ -1,53 +1,76 @@
 <?php
-include('classes/DB.php');
-include('classes/login.php');
+include('./classes/DB.php');
+include('./classes/Login.php');
+$tokenIsValid = False;
+if (Login::isLoggedIn()) {
 
-if (!login::isLoggedIn()) {
-    header("Location: login.php");
-}
+        if (isset($_POST['changepassword'])) {
 
-if(isset($_POST['changepassword']))
-{   // POST VARIABLES
-    $oldpassword = $_POST['oldpassword'];
-    $newpassword = $_POST['newpassword'];
-    $newpasswordrepeat = $_POST['newpasswordrepeat'];
+                $oldpassword = $_POST['oldpassword'];
+                $newpassword = $_POST['newpassword'];
+                $newpasswordrepeat = $_POST['newpasswordrepeat'];
+                $userid = Login::isLoggedIn();
 
-    // DECLARING USER ID FROM ISLOGGEDIN FUNCTION
-    $userid = login::isLoggedIn();
+                if (password_verify($oldpassword, DB::query('SELECT password FROM users WHERE id=:userid', array(':userid'=>$userid))[0]['password'])) {
 
-    if(password_verify($oldpassword, DB::query('SELECT password FROM users WHERE id=:userid',  array(':userid'=>$userid))[0]['password'])) 
-    {   
-        if($newpassword == $newpasswordrepeat)
-        {
-            if(strlen($newpassword) >= 6 && strlen($newpassword) <= 60)
-            {
-                DB::query('UPDATE users SET password=:newpassword WHERE id=:userid', array(':newpassword'=>password_hash($newpassword, PASSWORD_BCRYPT), ':userid'=>$userid));
-                echo "password changed Succesfully";
-            } 
-            else
-            {
-                echo "Password must be 6-60 characters";
-            }
-        } 
-        else
-        {
-            echo "Passwords not match";
+                        if ($newpassword == $newpasswordrepeat) {
+
+                                if (strlen($newpassword) >= 6 && strlen($newpassword) <= 60) {
+
+                                        DB::query('UPDATE users SET password=:newpassword WHERE id=:userid', array(':newpassword'=>password_hash($newpassword, PASSWORD_BCRYPT), ':userid'=>$userid));
+                                        echo 'Password changed successfully!';
+                                }
+
+                        } else {
+                                echo 'Passwords don\'t match!';
+                        }
+
+                } else {
+                        echo 'Incorrect old password!';
+                }
+
         }
 
+} else {
+        if (isset($_GET['token'])) {
+        $token = $_GET['token'];
+        if (DB::query('SELECT user_id FROM password_tokens WHERE token=:token', array(':token'=>sha1($token)))) {
+                $userid = DB::query('SELECT user_id FROM password_tokens WHERE token=:token', array(':token'=>sha1($token)))[0]['user_id'];
+                $tokenIsValid = True;
+                if (isset($_POST['changepassword'])) {
 
-    } else {
-        echo "incorrect old password";
-    }
+                        $newpassword = $_POST['newpassword'];
+                        $newpasswordrepeat = $_POST['newpasswordrepeat'];
 
+                                if ($newpassword == $newpasswordrepeat) {
+
+                                        if (strlen($newpassword) >= 6 && strlen($newpassword) <= 60) {
+
+                                                DB::query('UPDATE users SET password=:newpassword WHERE id=:userid', array(':newpassword'=>password_hash($newpassword, PASSWORD_BCRYPT), ':userid'=>$userid));
+                                                echo 'Password changed successfully!';
+                                                DB::query('DELETE FROM password_tokens WHERE user_id=:userid', array(':userid'=>$userid));
+                                        }
+
+                                } else {
+                                        echo 'Passwords don\'t match!';
+                                }
+
+                        }
+
+
+        } else {
+                die('Token invalid');
+        }
+} else {
+        die('Not logged in');
+}
 }
 
-
-
 ?>
-
-<form action="change-password.php" method="POST">
-    <input type="password" name="oldpassword" value="" placeholder="Current password"> </p>
-    <input type="password" name="newpassword" value="" placeholder="New password"> </p>
-    <input type="password" name="newpasswordrepeat" value="" placeholder="Repeat new password"> </p>
-    <input type="submit" name="changepassword" value="Change password">
+<h1>Change your Password</h1>
+<form action="<?php if (!$tokenIsValid) { echo 'change-password.php'; } else { echo 'change-password.php?token='.$token.''; } ?>" method="post">
+        <?php if (!$tokenIsValid) { echo '<input type="password" name="oldpassword" value="" placeholder="Current Password ..."><p />'; } ?>
+        <input type="password" name="newpassword" value="" placeholder="New Password ..."><p />
+        <input type="password" name="newpasswordrepeat" value="" placeholder="Repeat Password ..."><p />
+        <input type="submit" name="changepassword" value="Change Password">
 </form>
---
+++ profile.php
@@ -0,0 +1,61 @@
+<?php
+include('classes/DB.php');
+include('classes/login.php');
+
+$username = "";
+$isFollowing = false;
+
+if(isset($_GET['username'])){
+    if(DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username'])))
+    {
+
+        $username = (DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username'])))[0]['username'];
+        $userid = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
+        $followerid = login::isLoggedIn();
+
+
+        if(isset($_POST['follow'])){
+
+            if(!DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))){
+                DB::query('INSERT INTO followers VALUES (\'\', :userid, :followerid)', array(':userid'=>$userid, ':followerid'=>$followerid));
+            } else {
+                echo "ALREADY FOLLOWING";
+            }
+            $isFollowing = true;
+        }
+
+        if(isset($_POST['unfollow'])){
+
+            if(DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))){
+                DB::query('DELETE FROM followers WHERE user_id=:userid AND follower_id=:followerid', array(':userid'=>$userid, ':followerid'=>$followerid));
+            } else {
+                echo "ALREADY FOLLOWING";
+            }
+            $isFollowing = true;
+        }
+
+
+        if(DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid)))
+        {
+           // echo "ALREADY FOLLOWING";
+            $isFollowing = true;
+        }
+        
+    } else {
+        die("user not found");
+    }
+}
+?>
+
+<h1><?php echo $username?> sProfile</h1>
+
+
+<form action="profile.php?username=<?php echo $username;?>" method="POST">
+    <?php 
+    if($isFollowing){
+        echo '<input type="submit" name="unfollow" value="Unfollow">';
+    } else {
+        echo '<input type="submit" name="follow" value="Follow">';
+    }
+    ?>
+</form>
