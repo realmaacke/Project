8cdd1e3d97e06176819d09cc8eb8e3a8f5be9ad0
realmaacke <mackan.pettersson1@outlook.com>
2021-12-29 11:06:08 +0100

    sql

--- project.sql
+++ project.sql
@@ -32,12 +32,6 @@ CREATE TABLE `administrator` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `administrator`
---
-
-INSERT INTO `administrator` (`id`, `user_id`) VALUES
-(5, 3);
 
 -- --------------------------------------------------------
 
@@ -53,24 +47,6 @@ CREATE TABLE `comments` (
   `post_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `comments`
---
-
-INSERT INTO `comments` (`id`, `comment`, `user_id`, `posted_at`, `post_id`) VALUES
-(75, 'Test Comment', 3, 2147483647, 22),
-(76, 'Test Comment', 3, 2147483647, 22),
-(77, 'Test Comment', 3, 2147483647, 22),
-(81, 'dsa', 3, 2147483647, 22),
-(152, 'sad', 3, 2147483647, 22),
-(153, 'sa', 3, 2147483647, 22),
-(155, 's', 3, 2147483647, 2),
-(156, 'f', 3, 2147483647, 2),
-(157, 'g', 3, 2147483647, 3),
-(158, 'g', 3, 2147483647, 3),
-(159, 'g', 3, 2147483647, 3),
-(160, 's', 3, 2147483647, 22),
-(161, 'g', 3, 2147483647, 22);
 
 -- --------------------------------------------------------
 
@@ -84,14 +60,6 @@ CREATE TABLE `followers` (
   `follower_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `followers`
---
-
-INSERT INTO `followers` (`id`, `user_id`, `follower_id`) VALUES
-(62, 9, 3),
-(67, 6, 4),
-(88, 5, 3);
 
 -- --------------------------------------------------------
 
@@ -105,18 +73,6 @@ CREATE TABLE `login_tokens` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `login_tokens`
---
-
-INSERT INTO `login_tokens` (`id`, `token`, `user_id`) VALUES
-(11, 'f43a0571dae0d4f78ed2d9c1689842e73431b855', 6),
-(60, 'bba46729f2105a186fce519b5fc8c8fcf175df12', 9),
-(64, '27d11c471d8523ca115dcc02db3271ce485bbb75', 4),
-(65, '6b4980f4680ca984585c56396d6c0b6a8b3cd9f3', 3),
-(66, 'c42d639703e13b9f0fc8440a86544ced1708cadf', 3),
-(67, '98a30ad990a6d06d7086f16e922bca565485d0c7', 3),
-(68, '245f7c5ab1b323fab3dde23abb4736c2a5d8c940', 3);
 
 -- --------------------------------------------------------
 
@@ -131,23 +87,6 @@ CREATE TABLE `messages` (
   `msg` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `messages`
---
-
-INSERT INTO `messages` (`msg_id`, `outgoing_msg_id`, `incoming_msg_id`, `msg`) VALUES
-(1, 3, 11, 'asd'),
-(2, 3, 11, 'dgdhgf'),
-(3, 3, 11, 'ds'),
-(4, 3, 11, 'fds'),
-(5, 3, 11, 'hgj324'),
-(6, 4, 3, 'hej'),
-(7, 3, 4, 'hej'),
-(8, 4, 3, '123'),
-(9, 3, 4, 'asd'),
-(10, 3, 4, 'asd'),
-(11, 4, 11, 'asd'),
-(12, 3, 6, 'gvdsx');
 
 -- --------------------------------------------------------
 
@@ -163,14 +102,6 @@ CREATE TABLE `notifications` (
   `extra` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `notifications`
---
-
-INSERT INTO `notifications` (`id`, `type`, `receiver`, `sender`, `extra`) VALUES
-(1, 1, 3, 6, ''),
-(2, 1, 6, 6, '');
-
 -- --------------------------------------------------------
 
 --
@@ -183,15 +114,6 @@ CREATE TABLE `password_tokens` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `password_tokens`
---
-
-INSERT INTO `password_tokens` (`id`, `token`, `user_id`) VALUES
-(1, 2, 3),
-(2, 0, 3),
-(3, 9350, 3),
-(4, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -209,18 +131,6 @@ CREATE TABLE `posts` (
   `topics` varchar(400) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `posts`
---
-
-INSERT INTO `posts` (`id`, `body`, `posted_at`, `user_id`, `likes`, `postimg`, `topics`) VALUES
-(2, 'Verified', '2021-09-28 17:02:21', 5, 1, '', ''),
-(3, 'Sup', '2021-09-28 17:02:28', 5, 1, '', ''),
-(22, 'First Post #PHP #CODING', '2021-09-29 15:47:07', 6, 1, '', 'PHP,CODING,'),
-(53, '', '2021-11-26 18:00:52', 0, 0, '', ''),
-(54, 'ads', '2021-11-27 01:06:55', 3, 1, '', ''),
-(59, 'sad', '2021-11-28 12:56:49', 3, 1, '', ''),
-(60, 'asd #PHP', '2021-11-28 15:29:18', 4, 0, '', 'PHP,');
 
 -- --------------------------------------------------------
 
@@ -234,17 +144,6 @@ CREATE TABLE `post_likes` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `post_likes`
---
-
-INSERT INTO `post_likes` (`id`, `post_id`, `user_id`) VALUES
-(370, 0, 3),
-(387, 54, 3),
-(391, 3, 3),
-(393, 59, 3),
-(395, 2, 3),
-(401, 22, 3);
 
 -- --------------------------------------------------------
 
@@ -258,12 +157,6 @@ CREATE TABLE `topics` (
   `topics` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `topics`
---
-
-INSERT INTO `topics` (`id`, `post_id`, `topics`) VALUES
-(3, 0, 'PHP,');
 
 -- --------------------------------------------------------
 
@@ -281,20 +174,6 @@ CREATE TABLE `users` (
   `colorbanner` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumpning av Data i tabell `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `verified`, `profileimg`, `colorbanner`) VALUES
-(3, 'marcus', '$2y$10$50J7X/rb/tAMTP3V6VhEQusaSLrHkdGGWDm3fjYO1OvJeo6ROXsdq', 'mackan1@telia.com', 1, 'https://i.imgur.com/oQwrodB.png', '#8000ff'),
-(4, 'maacke', '$2y$10$xZXhCRuPyPxKfjLEmXQY0eKuvHQH2lpqEDSwfpXxW2e5C/Vp4cJBO', 'mackan1@gmail.com', 1, '', '#80f269'),
-(5, 'Verified', '$2y$10$uayWyzjduY770wwLY1wbXeGlQnf99dxiOiExmmujsN3DvsxaK9dzK', 'Verified@combined.com', 1, '', ''),
-(6, 'pettersson', '$2y$10$19W6Z87DlN/RDomgYnZS0uY5b/.6iI47MF9R7aWnI5OQRgD5vJmTi', 'pettersson1@telia.com', 0, 'https://i.imgur.com/A7eBOen.jpg', ''),
-(7, 'asd', '$2y$10$GXPuzIT/8xDhwCKHwNbx.eBYBBeGddO.Zae.zpeDagr9rlBvoZC.u', 'ads@gmail.com', 0, '', ''),
-(8, 'pettersson123', '$2y$10$vsisy4OCTxTBWwG9Et1bN.Z3RShWSEYjyo3rAEKFCcm0n8e3bFipG', 'marcus321@telia.com', 0, '', ''),
-(9, 'Guest', '$2y$10$H9tSfsenuBzHJ2tx/y9fn.WDQtoTxA3b29zqPaiDgA7ICUWrhm672', 'guest@gmail.com', 0, '', ''),
-(10, 'Fearless', '$2y$10$h7ClsWTQ/zGjnFpb/RKFeePqyqNKdetDpnqGT1mkS3QGI2YYrpe6a', 'Fearless@gmail.com', 0, '', ''),
-(11, 'dfhgdfs', '$2y$10$KWmwuCrWNwa1coGSRElMwu/pjoOw7fzlAyNnSjfiwaq5QazGb84QG', 'marcus1@telia.com', 0, '', '');
 
 --
 -- Index för dumpade tabeller
