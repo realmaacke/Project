4d92832eb91d751b728efe269f4a424606a12f37
realmaacke <mackan.pettersson1@outlook.com>
2022-01-14 17:10:11 +0100

    friend list added

--- classes/user.php
+++ classes/user.php
@@ -11,4 +11,36 @@ class User{
     {
         return DB::query('SELECT * FROM users WHERE username=:username', array(':username'=>$name));
     }
+
+    public static function displayFriendslist($userid)
+    {
+        $data = DB::query('SELECT * FROM followers WHERE user_id=:userid',array(':userid'=>$userid));
+
+        foreach($data as $d)
+        {
+            $user = DB::query('SELECT * FROM users WHERE id=:dataid',array(':dataid'=>$d['follower_id']));
+            ?>
+            <div class="friends-item">
+
+                <div class="image-div">
+
+                    <?php echo Profile::displayImage($user[0]['username'], false) ?>
+                    
+                </div>
+
+                <div class="contact-div">
+
+                    <div class="contact-name">
+
+                        <h1><a href='profile.php?username=<?php echo $user[0]['username']; ?>'>@<?php echo $user[0]['username'];?></a></h1>
+                        <p><?php echo strtoupper($user[0]['username']); ?></p>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <?php
+        }
+    }
 }
--- friendlist.php
+++ friendlist.php
@@ -1,3 +1,20 @@
+<?php
+
+include('autoload.php');  // loading all classes using spl loader
+
+if (Login::isLoggedIn())  
+{
+    $userid = Login::isLoggedIn(); 
+}
+else 
+{  
+    Redirect::goto('login.php'); 
+}
+    $isAdmin = authorization::ValidateAdmin($userid);
+    $friends = DB::query('SELECT * FROM followers WHERE user_id=:userid', array(':userid'=>$userid));
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +23,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Visual/style.css">
+    <link rel="stylesheet" href="Visual/friendlist.css">
        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
        <link rel="icon" type="image/x-icon" href="Visual\img\favicon.ico">
     <script src="https://kit.fontawesome.com/6bfb37676a.js" crossorigin="anonymous"></script>
     <title>COMBINED FRIENDLIST</title>
 </head>
 <body>
-    
+    <?php include 'navigation.php';  ?>
+
+    <div class="content">
+            <div class="list-div">
+                <div class="header-text">
+                    <h1>FRIENDS (<?php echo count($friends) ?>)</h1>
+                </div>
+
+                <div class="friend-list">
+                <!-- START -->
+                <?php echo User::displayFriendslist($userid); ?>
+                <!-- END -->
+                </div>
+            </div>
+    </div>
 </body>
 </html>
