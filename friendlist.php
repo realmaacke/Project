8a804eb211e5d2ec14d4cff31d5af17f626a1825
realmaacke <mackan.pettersson1@outlook.com>
2022-01-14 16:03:48 +0100

    friendlist

---
+++ friendlist.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Visual/style.css">
+       <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+       <link rel="icon" type="image/x-icon" href="Visual\img\favicon.ico">
+    <script src="https://kit.fontawesome.com/6bfb37676a.js" crossorigin="anonymous"></script>
+    <title>COMBINED FRIENDLIST</title>
+</head>
+<body>
+    
+</body>
+</html>
